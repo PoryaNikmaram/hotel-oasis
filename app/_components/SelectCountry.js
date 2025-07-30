9e493724b358ead5484db570d70215acd0d36962
PoryaNikmaram <nikmaramp@gmail.com>
2025-07-30 11:59:21 +0330

    fix(components): linting and formatting pages structures

--- app/_components/SelectCountry.js
+++ app/_components/SelectCountry.js
@@ -1,9 +1,9 @@
-import { getCountries } from "@/app/_lib/data-service";
+import { getCountries } from '../_lib/data-service';
 
 async function SelectCountry({ defaultCountry, name, id, className }) {
   const countries = await getCountries();
   const flag =
-    countries.find((country) => country.name === defaultCountry)?.flag ?? "";
+    countries.find(country => country.name === defaultCountry)?.flag ?? '';
 
   return (
     <select
@@ -12,8 +12,8 @@ async function SelectCountry({ defaultCountry, name, id, className }) {
       defaultValue={`${defaultCountry}%${flag}`}
       className={className}
     >
-      <option value="">Select country...</option>
-      {countries.map((c) => (
+      <option value=''>Select country...</option>
+      {countries.map(c => (
         <option key={c.name} value={`${c.name}%${c.flag}`}>
           {c.name}
         </option>
