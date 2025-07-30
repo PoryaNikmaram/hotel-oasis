5e0389fa6e375c793eb04d6aec9b3d5dd61f4db5
PoryaNikmaram <nikmaramp@gmail.com>
2025-07-30 11:59:35 +0330

    fix(app): linting and formatting pages structures

--- app/layout.js
+++ app/layout.js
@@ -1,30 +1,32 @@
-import Header from "./_components/Header";
-import "@/app/_styles/globals.css";
-import { Josefin_Sans } from "next/font/google";
+import { Josefin_Sans } from 'next/font/google';
+
+import Header from './_components/Header';
+
+import '@/app/_styles/globals.css';
 
 const josefin = Josefin_Sans({
-  subsets: ["latin"],
-  display: "swap",
+  subsets: ['latin'],
+  display: 'swap'
 });
 
 export const metadata = {
   title: {
-    template: "%s / The Wild Oasis",
-    default: "Welcome / The Wild Oasis",
+    template: '%s / The Wild Oasis',
+    default: 'Welcome / The Wild Oasis'
   },
-  discription: "Luxurios cabin hotel, located in the heart of the italy",
+  discription: 'Luxurios cabin hotel, located in the heart of the italy'
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
-        className={` ${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased`}
+        className={` ${josefin.className} flex min-h-screen flex-col bg-primary-950 text-primary-100 antialiased`}
       >
         <Header />
 
-        <div className="flex-1 px-8 py-12 grid">
-          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        <div className='grid flex-1 px-8 py-12'>
+          <main className='mx-auto w-full max-w-7xl'>{children}</main>
         </div>
       </body>
     </html>
